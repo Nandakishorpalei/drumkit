e9ea1b0cf0549d0df29154d36a309e1b3ffa1a19
Nandakishorpalei <nandakishorpalei7676@gmail.com>
2020-12-12 12:19:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,52 +21,102 @@ function makesound(key){
   switch(key){
     case "w":
 
-var audio=new Audio("sounds/crash.mp3");
+var audio=new Audio("crash.mp3");
 audio.play();
 
 break;
 
   case "a":
 
-var audio1=new Audio("sounds/kick-bass.mp3");
+var audio1=new Audio("kick-bass.mp3");
 audio1.play();
 
 break;
 
   case "s":
 
-var audio2=new Audio("sounds/snare.mp3");
+var audio2=new Audio("snare.mp3");
 audio2.play();
 
 break;
 
   case "d":
 
-var audio3=new Audio("sounds/tom-1.mp3");
+var audio3=new Audio("tom-1.mp3");
 audio3.play();
 
 break;
 
   case "j":
 
-var audio4=new Audio("sounds/tom-2.mp3");
+var audio4=new Audio("tom-2.mp3");
 audio4.play();
 
 break;
 
   case "k":
 
-var audio5=new Audio("sounds/tom-3.mp3");
+var audio5=new Audio("tom-3.mp3");
 audio5.play();
 
 break;
 
   case "l":
 
-var audio6=new Audio("sounds/tom-4.mp3");
+var audio6=new Audio("tom-4.mp3");
 audio6.play();
 
   break;
+      
+        case "W":
+
+var audio7=new Audio("crash.mp3");
+audio7.play();
+
+break;
+
+  case "A":
+
+var audio8=new Audio("kick-bass.mp3");
+audio8.play();
+
+break;
+
+  case "S":
+
+var audio9=new Audio("snare.mp3");
+audio9.play();
+
+break;
+
+  case "D":
+
+var audio10=new Audio("tom-1.mp3");
+audio10.play();
+
+break;
+
+  case "J":
+
+var audio11=new Audio("tom-2.mp3");
+audio11.play();
+
+break;
+
+  case "K":
+
+var audio12=new Audio("tom-3.mp3");
+audio12.play();
+
+break;
+
+  case "L":
+
+var audio13=new Audio("tom-4.mp3");
+audio13.play();
+
+  break;
+
 
  default:
  alert("wrong input");
